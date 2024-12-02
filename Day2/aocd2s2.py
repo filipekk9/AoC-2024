14fdfbdbbc61ca0a6e8f87213f1ae1985781bca8
filipekk9 <filipo6800@o2.pl>
2024-12-02 11:09:40 +0100

    upd

---
+++ Day2/aocd2s2.py
@@ -0,0 +1,5 @@
+input = [list(map(int,x.split(" "))) for x in open("test_input2.txt", "r").read().splitlines()]
+safe = 0
+
+
+
--- aocd2s2.py
+++
@@ -1,3 +0,0 @@
-input = [list(map(int,x.split(" "))) for x in open("input2.txt", "r").read().splitlines()]
-safe = 0
-
